672b05b1644a06137499e4161d9e3250959f8448
Kyler Nguyen <tkien2703@gmail.com>
2024-10-01 13:43:20 +0700

    install kafka.js

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
+
+  /* @todo: implement Kafka for scaling socket
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      }
+    }
+  });
+  */
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'src', 'static'));
